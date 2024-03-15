72930ff0eac2ab05c580d0d2904dcf83cbd45928
23012310 <163557406+23012310@users.noreply.github.com>
2024-03-15 21:10:09 +0200

    Add files via upload

--- main.cpp
+++ main.cpp
@@ -4,6 +4,37 @@ using namespace std;
 
 int main()
 {
-    cout << "Hello world!" << endl;
+    double totalCost,salary,yearlyRent,electricityCost;
+
+    cout << "enter the total cost of the merchandise"<<endl;
+    cin>>totalCost;
+
+    cout<< "enter the salary of the employees including your salary"<<endl;
+    cin>>salary;
+
+    cout<< "enter the yearly rent"<<endl;
+    cin>>yearlyRent;
+
+    cout<< "enter the electricity cost"<<endl;
+    cin>>electricityCost;
+
+
+     double totalExpenses = totalCost+salary+yearlyRent+electricityCost;
+     double desiredNetProfit=totalCost*0.1;
+     double newNetProfit=desiredNetProfit/0.85;
+     double markUp=(newNetProfit/totalExpenses)/totalCost;
+
+     cout<<"the markup is "<<markUp<<endl;
+
     return 0;
+
+
+
+
+
+
+
+
+
+
 }
